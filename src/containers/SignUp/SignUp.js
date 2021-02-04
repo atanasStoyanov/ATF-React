c7abfa24a8a22b56a55280c3f11efea5bbb397eb
Stoyanov <atanass@dxc.com>
2021-02-04 22:55:38 +0200

    fix

--- src/containers/SignUp/SignUp.js
+++ src/containers/SignUp/SignUp.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
-import PageLayout from '../PageLayout/PageLayout';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import styles from './SignUp.module.css';
+import PageLayout from '../PageLayout/PageLayout';
 
-// import Input from '../../components/Input/Input';
 import InputEl from '../../components/Input/InputEl';
 import SubmitButton from '../../components/Button/SubmitButton';
-import {updateObject} from '../../utils/updateObject';
-import {checkValidity} from '../../utils/checkValidity';
+import Spinner from '../../components/Spinner/Spinner';
+import { updateObject } from '../../utils/updateObject';
+import { checkValidity } from '../../utils/checkValidity';
 
 const SignUp = props => {
-
-    // const [email, setEmail] = useState('');
-    // const [password, setPassword] = useState('');
-    // const [RePassword, setRePassword] = useState('');
-    // const [errorMsg, setErrorMsg] = useState('');
 
     const [formElements, setFormElements] = useState(
         {
@@ -63,22 +60,50 @@ const SignUp = props => {
                 touched: false
             }
         }
-    )
+    );
+
+    const [errorMsg, setErrorMsg] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const history = useHistory();
 
     const inputChangedHandler = (event, element) => {
         const updatedElements = updateObject(formElements, {
             [element]: updateObject(formElements[element], {
                 value: event.target.value,
-                valid:checkValidity(event.target.value, formElements[element].validation),
+                valid: checkValidity(event.target.value, formElements[element].validation),
                 touched: true
             })
         });
 
-        setFormElements(updatedElements); 
+        setFormElements(updatedElements);
     }
 
     const handleSubmit = event => {
         event.preventDefault();
+        setLoading(true);
+
+        if (formElements.password.value !== formElements.rePassword.value) {
+            setErrorMsg('Passwords do not match');
+            return;
+        }
+
+        const authData = {
+            email: formElements.email.value,
+            password: formElements.password.value,
+            returnSecureToken: true
+        }
+
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA6Peyo5GN3spsTPYMadeQlfkp91rj6YMA', authData)
+            .then(response => {
+                setLoading(false);
+                console.log(response);
+                history.push('/');
+            })
+            .catch(err => {
+                setLoading(false);
+                setErrorMsg(err.response.data.error.message);
+            });
 
     }
 
@@ -91,7 +116,7 @@ const SignUp = props => {
         });
     }
 
-    let form = formElementsArray.map(formElement =>(
+    let form = formElementsArray.map(formElement => (
         <InputEl
             key={formElement.id}
             label={formElement.config.label}
@@ -103,50 +128,18 @@ const SignUp = props => {
             touched={formElement.config.touched}
             changed={event => inputChangedHandler(event, formElement.id)}
         />
-    ))
+
+    ));
+
 
     return (
         <PageLayout>
             <div className={styles.Media}>
                 <form className={styles.Form} onSubmit={handleSubmit}>
                     <h3>Create an Account</h3>
-                    {form}
-                    {/* <Input
-                        label='Email'
-                        id='email'
-                        type='email'
-                        value={email}
-                        onChange={e => {
-                            setEmail(e.target.value);
-                            setErrorMsg(null);
-                        }}
-                        placeholder='Please enter valid email'
-                    />
-                    <Input
-                        label='Password'
-                        id='password'
-                        type='password'
-                        value={password}
-                        onChange={e => {
-                            setPassword(e.target.value);
-                            setErrorMsg(null);
-                        }}
-                        placeholder='******'
-                    />
-                    <Input
-                        label='Re-Password'
-                        id='re-password'
-                        type='password'
-                        value={RePassword}
-                        onChange={e => {
-                            setRePassword(e.target.value);
-                            setErrorMsg(null);
-                        }}
-                        placeholder='******'
-                    /> */}
-
-
-                    <SubmitButton title='Create Account'/>
+                    {loading ? <Spinner/> : form}
+                    <p className={styles.ErrorMsg}>{errorMsg}</p>
+                    <SubmitButton title='Create Account' />
                 </form>
             </div>
         </PageLayout>
