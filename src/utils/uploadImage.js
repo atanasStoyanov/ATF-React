be86602e42272d58c3df69852cb82ee5a72845db
Stoyanov <atanass@dxc.com>
2021-02-16 17:47:18 +0200

    fix: upload image

--- src/utils/uploadImage.js
+++ src/utils/uploadImage.js
@@ -1,12 +1,15 @@
-import axios from 'axios';
-
 export const uploadImage = async (files, url, folder) => {
     const data = new FormData();
     data.append('file', files[0]);
     data.append('upload_preset', folder);
 
+    const res = await fetch(url, {
+        method: 'POST',
+        body: data
+    });
 
-    const file = await axios.post(url, data);
+    const file = await res.json();
+
     const imageUrl = file.secure_url;
     
    return imageUrl;
